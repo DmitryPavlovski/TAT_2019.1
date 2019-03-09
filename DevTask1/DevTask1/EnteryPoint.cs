e18eef0b1b41f9bf14764864bd86ab4e4e87be15
Дмитрий Павловский <you@example.com>
2019-03-10 01:15:00 +0300

    add Task_DEV-1

---
+++ DevTask1/DevTask1/EnteryPoint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTask1
+{
+    class EnteryPoint
+    {
+        /// <summary>
+     /// The program accepts the sequence and displays
+     /// all subsequences without consecutively repeating symdols
+     /// </summary>
+     /// <param name="args">The command line arguments</param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (ChekForEmptiness(args))
+                {
+                    throw new Exception("String too short!");
+                }
+                SubStrings line = new SubStrings(args);
+                line.SearchSubstrings();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        private static bool ChekForEmptiness(string[] str)
+        {
+            return str.Length == 0 || str[0].Length <= 2;
+        }
+    }
+}
---
+++ DevTask1/DevTask1/SubStrings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTask1
+{
+    class SubStrings
+    {
+        StringBuilder str;
+        /// <summary>
+        /// Constructor forms a string from array of strings
+        /// </summary>
+        /// <param name="str">Array of strings</param>
+        public SubStrings(string[] str)
+        {
+            this.str = new StringBuilder();
+            foreach (string word in str)
+            {
+                this.str.Append(word).Append(" ");
+            }
+            this.str.Remove(this.str.Length - 1, 1);
+
+        }
+        ///<summary>
+        /// the method takes a string and displays all the substrings.
+        /// </summary>
+        public void SearchSubstrings()
+        {
+            String str = String.Join("", this.str);
+            for (int firstElement = 0; firstElement < str.Length; firstElement++)
+            {
+                for (int lastElevent = firstElement; lastElevent <= str.Length - 1; lastElevent++)
+                {
+                    if (lastElevent - firstElement > 0)
+                        Console.WriteLine(str.Substring(firstElement, lastElevent - firstElement + 1));
+                    if (str.Length - lastElevent > 1
+                        && str[lastElevent] == str[lastElevent + 1]) break;
+                }
+            }
+        }
+    }
+}
