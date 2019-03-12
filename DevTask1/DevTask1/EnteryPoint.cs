2d2e5ca77d91fc53e135e4fdfa75c798738665fa
Дмитрий Павловский <you@example.com>
2019-03-12 21:04:36 +0300

    constructor fix and division of one method into two simple

--- DevTask1/DevTask1/EnteryPoint.cs
+++ DevTask1/DevTask1/EnteryPoint.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DevTask1
 {
@@ -16,22 +12,14 @@ namespace DevTask1
         static void Main(string[] args)
         {
             try
-            {
-                if (ChekForEmptiness(args))
-                {
-                    throw new Exception("String too short!");
-                }
+            {                
                 SubStrings line = new SubStrings(args);
-                line.SearchSubstrings();
+                line.DisplayAndSearchAllSubstrings();
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}");
             }
-        }
-        private static bool ChekForEmptiness(string[] str)
-        {
-            return str.Length == 0 || str[0].Length <= 2;
-        }
+        }        
     }
 }
--- DevTask1/DevTask1/SubStrings.cs
+++ DevTask1/DevTask1/SubStrings.cs
@@ -1,44 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DevTask1
 {
     class SubStrings
     {
-        StringBuilder str;
+        private StringBuilder str;
         /// <summary>
         /// Constructor forms a string from array of strings
         /// </summary>
         /// <param name="str">Array of strings</param>
         public SubStrings(string[] str)
+        {           
+                if (ChekForEmptiness(str))
+                {
+                    throw new Exception("String too short!");
+                }
+                this.str = new StringBuilder();
+                foreach (string word in str)
+                {
+                    this.str.Append(word).Append(" ");
+                }
+                this.str.Remove(this.str.Length - 1, 1);            
+        }
+        private bool ChekForEmptiness(string[] str)
         {
-            this.str = new StringBuilder();
-            foreach (string word in str)
+            return str.Length == 0 || str[0].Length <= 2;
+        }
+        public string SearchNextSubstring(string str, int indexFirstElement, int indexPreviousElement)
+        {
+            if (str[indexPreviousElement] == str[indexPreviousElement + 1])
             {
-                this.str.Append(word).Append(" ");
+                return "0";
             }
-            this.str.Remove(this.str.Length - 1, 1);
-
+            return str.Substring(indexFirstElement, indexPreviousElement - indexFirstElement + 2);           
         }
         ///<summary>
         /// the method takes a string and displays all the substrings.
         /// </summary>
-        public void SearchSubstrings()
+        public void DisplayAndSearchAllSubstrings()
         {
-            String str = String.Join("", this.str);
             for (int firstElement = 0; firstElement < str.Length; firstElement++)
             {
-                for (int lastElevent = firstElement; lastElevent <= str.Length - 1; lastElevent++)
+                for (int lastElevent = firstElement; lastElevent < str.Length - 1; lastElevent++)
                 {
-                    if (lastElevent - firstElement > 0)
-                        Console.WriteLine(str.Substring(firstElement, lastElevent - firstElement + 1));
-                    if (str.Length - lastElevent > 1
-                        && str[lastElevent] == str[lastElevent + 1]) break;
+                    if(SearchNextSubstring( this.str.ToString(), firstElement, lastElevent).Length>1)
+                    {
+                        Console.WriteLine(SearchNextSubstring(this.str.ToString(),firstElement, lastElevent));
+                    }
+                    if (str.Length - lastElevent > 1 && str[lastElevent] == str[lastElevent + 1])
+                    {
+                        break;
+                    }
                 }
             }
         }
+       
     }
 }
