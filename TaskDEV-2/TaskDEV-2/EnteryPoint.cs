1fcde1bcc657eccc271467f7e2224c75c7508ff6
Дмитрий Павловский <you@example.com>
2019-03-14 18:32:48 +0300

    TODO: word reading method

---
+++ TaskDEV-2/TaskDEV-2/EnteryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDEV_2
+{
+    class EnteryPoint
+    {
+        static void Main()
+        {
+            try
+            {
+                var str = Console.ReadLine();
+                var word = new Phonetics(str);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: Something happened");
+            }
+        }
+    }
+}
---
+++ TaskDEV-2/TaskDEV-2/Phonetics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDEV_2
+{
+    class Phonetics
+    {
+        private StringBuilder word;
+
+        public Phonetics(string str)
+        {
+            word = new StringBuilder();
+            word.Append(str);
+        }
+        string ParseOnSounds()
+        {
+            StringBuilder phoneticsWord;
+            for(int i=0; i<word.Length;i++)
+            {
+                // if (word[i]==) TODO: word reading method
+            }
+            return string.Empty;//phoneticsWord.ToString();
+        }
+    }
+}
