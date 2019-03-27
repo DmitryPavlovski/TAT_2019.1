18e04d517e0c85c613598855d00f6319ef436395
Дмитрий Павловский <you@example.com>
2019-03-27 14:15:11 +0300

    add second point

--- Task_DEV-4/Task_DEV-4/Description.cs
+++ Task_DEV-4/Task_DEV-4/Description.cs
@@ -7,62 +7,32 @@ using System.Threading.Tasks;
 
 namespace Task_DEV_4
 {
-    class Description
+    class Description : Id
     {
-        private string information;
-        public string Information
+        private string textInformation;
+        public string TextInformation
         {
             get
             {
-                return information;
+                return textInformation;
             }
             set
             {
-                if (Information.Length > 256)
+                if (TextInformation.Length > 256)
                 {
                     Console.WriteLine("Description should be less");
-                    information = string.Empty;
+                    textInformation = string.Empty;
                 }
                 else
                 {
-                    information = value;
+                    textInformation = value;
                 }
                     
             }
         }
         public Description(int size)
         {
-            Information = GetText(size);
+            TextInformation = GetText(size);
         }
-        public string GetText(int restriction = 50)
-        {
-            StringBuilder line = new StringBuilder();
-            StreamReader reader = new StreamReader("../../Description.txt");
-
-
-            string partLine = reader.ReadLine();
-            if (partLine == null)
-            {
-                return null;
-            }
-            while (partLine != null)
-            {             
-                line.Append(partLine);
-                partLine = reader.ReadLine();
-            }
-            reader.Close();
-
-            string text = line.ToString();
-            var random = new Random();
-            int indexFirstElement, indexLastElement;
-            indexFirstElement = random.Next(0, text.Length);
-            indexLastElement = random.Next(indexFirstElement + 1, indexFirstElement + restriction);
-            if (indexLastElement > text.Length)
-            {
-                indexLastElement = text.Length - 1;
-            }
-            return text.Substring(indexFirstElement, indexLastElement - indexFirstElement);
-        }
-
     }
 }
