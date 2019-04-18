196bb7886f7faf8e7106db88f6793b27c17f2f14
Дмитрий Павловский <pavdim26@gmail.com>
2019-04-18 14:22:16 +0300

    fix code style

---
+++ TaskDEV-2/TaskDEV-2/Symbol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskDEV_2
+{
+    class Symbol
+    {
+    }
+}
